66770d1aef4a38fc1dfbf824dd20948b411b0a80
Srdjan Milovanov <s.milovanov@gmail.com>
2016-12-05 22:14:20 +0100

    fixed data-dismiss typeO :)

--- AjaxStarksApplication/App_Start/BundleConfig.cs
+++ AjaxStarksApplication/App_Start/BundleConfig.cs
@@ -9,10 +9,7 @@ namespace AjaxStarksApplication
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        //"~/Scripts/jquery-{version}.js"));
                         "~/Scripts/jquery.min.js"
-               
-
                         ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
@@ -24,18 +21,17 @@ namespace AjaxStarksApplication
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      
                       "~/Scripts/bootstrap.min.js",
                       "~/Scripts/respond.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
                 "~/Scripts/jquery-ui.min.js"
                 ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
                       "~/Content/bootstrap.min.css",
                       "~/Content/jquery-ui.min.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 }
